538bad9a973a20e7e1f326d3bfa95b8ce3b94dc6
dannyfrelink <danny-frelink@hotmail.com>
2022-03-14 12:09:49 +0100

    add local json file as fetch

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ fetch('../voeding.json', config)
   .catch(err => console.log(err));
 
 // render data
-function render(data) {
+const render = (data) => {
   const results = data;
   console.dir(results);
   results.forEach((item, i) => {
@@ -33,7 +33,3 @@ function render(data) {
     main.insertAdjacentHTML('beforeend', html);
   });
 }
-
-// const url = 'https://zoeken.oba.nl/api/v1/search/?q=%22voeding%22&refine=true&authorization=16c19e6083308c984c452600134989ba&sort=act_dt_asc&output=json' //&branch=OBA%20geuzenveld'
-// fetch(url).then(response => response.json())
-//   .then(data => console.table(data.results))
