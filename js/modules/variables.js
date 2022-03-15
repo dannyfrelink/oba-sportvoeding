ae737f1f05124603113eacf248f2cb1e9103ae6c
dannyfrelink <danny-frelink@hotmail.com>
2022-03-15 10:59:57 +0100

    new key

--- js/modules/variables.js
+++ js/modules/variables.js
@@ -1,7 +1,7 @@
 /* Fetch */
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const key = 'dc0e2f073c03758140452044906bc818';
+const key = '76f45dfa187d66be5fd6af05573eab04';
 const secret = '6b02878111c06660300798cf4c40a685';
 const detail = 'Default';
 const pagesize = '15';
